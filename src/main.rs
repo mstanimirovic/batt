30a6c64fed5945300e7bf28153c251bfc7ad29e0
Mladen Stanimirovic <mstanimirovic@proton.me>
2025-03-07 09:36:44 +0100

    script now prints out info for all power devices
    implemented structures for power devices
    added power consumption data

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,135 @@
-use std::collections::BTreeMap;
+#![allow(dead_code)]
 
-const UNUSED_KEYS: [&'static str; 9] = [
+use std::{
+    collections::BTreeMap,
+    fs,
+    path::{Path, PathBuf},
+};
+
+const UNUSED_KEYS: [&'static str; 11] = [
     "DEVTYPE",
-    "POWER_NOW",
-    "VOLTAGE_NOW",
-    "VOLTAGE_MIN_DESIGN",
-    "PRESENT",
-    "ENERGY_NOW",
-    "ENERGY_FULL",
-    "ENERGY_FULL_DESIGN",
-    "SERIAL_NUMBER",
+    "POWER_SUPPLY_POWER_NOW",
+    "POWER_SUPPLY_VOLTAGE_NOW",
+    "POWER_SUPPLY_VOLTAGE_MIN_DESIGN",
+    "POWER_SUPPLY_PRESENT",
+    "POWER_SUPPLY_ENERGY_NOW",
+    "POWER_SUPPLY_ENERGY_FULL",
+    "POWER_SUPPLY_ENERGY_FULL_DESIGN",
+    "POWER_SUPPLY_SERIAL_NUMBER",
+    "POWER_SUPPLY_NAME",
+    "POWER_SUPPLY_TYPE",
 ];
+
+#[derive(Debug, Clone)]
+struct PowerDevice {
+    name: String,
+    dtype: String,
+    data: BTreeMap<String, String>,
+}
+
+impl PowerDevice {
+    fn from(path: PathBuf) -> Self {
+        let data = parse_uevent(read_file(&path.join("uevent")));
+        let name = data.get("POWER_SUPPLY_NAME").unwrap().clone();
+        let dtype = data.get("POWER_SUPPLY_TYPE").unwrap().clone();
+        PowerDevice { name, dtype, data }
+    }
+
+    fn calc_data(&mut self) {
+        match self.data.get("POWER_SUPPLY_TYPE") {
+            Some(devtype) if devtype != &String::from("Battery") => return,
+            _ => {}
+        }
+
+        self.calc_health_percentage();
+        self.calc_power_consumption();
+    }
+
+    fn calc_health_percentage(&mut self) {
+        let energy_full = self
+            .data
+            .get("POWER_SUPPLY_ENERGY_FULL")
+            .unwrap_or(&String::from("0"))
+            .clone();
+
+        let energy_full_capacity = self
+            .data
+            .get("POWER_SUPPLY_ENERGY_FULL_DESIGN")
+            .unwrap_or(&String::from("0"))
+            .clone();
+
+        let health_percentage = calc_percentage(
+            energy_full.clone().parse().unwrap_or(0.0),
+            energy_full_capacity.clone().parse().unwrap_or(0.0),
+        );
+
+        self.data.insert(
+            String::from("POWER_SUPPLY_HEALTH_PERCENTAGE"),
+            health_percentage.to_string(),
+        );
+    }
+
+    fn calc_power_consumption(&mut self) {
+        let value = self
+            .data
+            .get("POWER_SUPPLY_POWER_NOW")
+            .unwrap_or(&String::from("0"))
+            .clone()
+            .parse()
+            .unwrap_or(0);
+
+        self.data.insert(
+            String::from("POWER_SUPPLY_POWER_CONSUMPTION"),
+            format!("{}", round_2d(value as f32 / 1_000_000.0)),
+        );
+    }
+
+    fn format_key(&self, mut key: String) -> String {
+        if key.len() > 13 {
+            key = key.split_off(13);
+        }
+        key.replace("_", " ").to_lowercase()
+    }
+
+    fn max_key_size(&self) -> usize {
+        self.data.keys().map(|k| k.len()).max().unwrap_or(0) - 10
+    }
+}
+
+impl std::fmt::Display for PowerDevice {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut output = String::new();
+        let max_key_size = self.max_key_size();
+
+        output.push_str(&format!("Device: {} {}\n", self.name, self.dtype));
+        for (key, value) in &self.data {
+            let formated_key = self.format_key(key.clone());
+            if UNUSED_KEYS.contains(&key.as_str()) {
+                continue;
+            }
+            output.push_str(&format!("{:<max_key_size$} = {}\n", formated_key, value));
+        }
+        write!(f, "{}", output)
+    }
+}
 
 fn calc_percentage(part: f32, total: f32) -> f32 {
     let percentage = (part / total) * 100.0;
     (percentage * 100.0).round() / 100.0
 }
 
-fn read_file(filename: &str) -> String {
-    match std::fs::read_to_string(filename) {
+fn round_2d(value: f32) -> f32 {
+    (value * 100.0).round() / 100.0
+}
+
+fn read_file(path: &PathBuf) -> String {
+    match std::fs::read_to_string(path) {
         Ok(content) => content.trim().to_string(),
         Err(error) => panic!("Failed to read file: {}", error),
     }
 }
 
-fn parse_uevent(uevent: &str) -> BTreeMap<String, String> {
+fn parse_uevent(uevent: String) -> BTreeMap<String, String> {
     let mut data = BTreeMap::new();
     for line in uevent.lines() {
         let parts: Vec<&str> = line.split('=').collect();
@@ -33,70 +138,28 @@ fn parse_uevent(uevent: &str) -> BTreeMap<String, String> {
         }
     }
 
-    // append percentage sign to capacity
-    let capacity = data.get_mut("POWER_SUPPLY_CAPACITY").unwrap();
-    capacity.push_str("%");
-
     // return the parsed data
     data
 }
 
-fn calc_health_percentage(data: &mut BTreeMap<String, String>) {
-    let energy_full = data
-        .get("POWER_SUPPLY_ENERGY_FULL")
-        .unwrap_or(&String::from("0"))
-        .clone();
+fn main() {
+    let power_supply = Path::new("/sys/class/power_supply");
+    if !power_supply.exists() {
+        println!("power_supply directory not found");
+        return;
+    }
 
-    let energy_full_capacity = data
-        .get("POWER_SUPPLY_ENERGY_FULL_DESIGN")
-        .unwrap_or(&String::from("0"))
-        .clone();
-
-    let health_percentage = calc_percentage(
-        energy_full.clone().parse().unwrap_or(0.0),
-        energy_full_capacity.clone().parse().unwrap_or(0.0),
-    );
-
-    data.insert(
-        String::from("POWER_SUPPLY_HEALTH_PERCENTAGE"),
-        health_percentage.to_string() + "%",
-    );
-}
-
-fn print_key_value(key: &String, value: &String, width: usize) {
-    let key = key.clone().replace("_", " ").to_lowercase();
-    print!("{:<width$}", &key);
-    println!("{}", value)
-}
-
-fn data_console_dump(data: &BTreeMap<String, String>) {
-    let longest_key = data
-        .keys()
-        .max_by_key(|k| k.len())
-        .unwrap_or(&String::from(""))
-        .clone();
-
-    let max_key_size = if longest_key.len() > 10 {
-        longest_key.len() - 10
-    } else {
-        10
-    };
-
-    for (key, value) in data {
-        let trimmed_key = key.clone().replace("POWER_SUPPLY_", "");
-        if !UNUSED_KEYS.contains(&trimmed_key.as_str()) {
-            print_key_value(&trimmed_key, value, max_key_size);
+    let mut devices: Vec<PowerDevice> = vec![];
+    if let Ok(entries) = fs::read_dir(power_supply) {
+        for entry in entries {
+            if let Ok(entry) = entry {
+                devices.push(PowerDevice::from(entry.path()));
+            }
         }
     }
-}
 
-fn main() {
-    // uevent file contains all the information about the battery
-    // KEY=VALUE in this specific format
-    let uevent = read_file("/sys/class/power_supply/BAT0/uevent");
-    let mut data: BTreeMap<String, String> = parse_uevent(&uevent);
-
-    // (ENERGY_FULL / ENERGY_FULL_DESIGN) * 100 = HEALTH_PERCENTAGE
-    calc_health_percentage(&mut data);
-    data_console_dump(&data);
+    for device in &mut devices {
+        device.calc_data();
+        println!("{}", device);
+    }
 }
