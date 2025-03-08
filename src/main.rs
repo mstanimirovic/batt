6da834aa3e817ca1fb4a20fc27251695966a6d96
Mladen Stanimirovic <mstanimirovic@proton.me>
2025-03-08 10:38:41 +0100

    added time remaining until empty (full)
    displays time remaining in hours (2.14)
    only for discharging and charging modes

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use std::{
     path::{Path, PathBuf},
 };
 
-const UNUSED_KEYS: [&'static str; 11] = [
+const UNUSED_KEYS: [&str; 11] = [
     "DEVTYPE",
     "POWER_SUPPLY_POWER_NOW",
     "POWER_SUPPLY_VOLTAGE_NOW",
@@ -28,7 +28,7 @@ struct PowerDevice {
 }
 
 impl PowerDevice {
-    fn from(path: PathBuf) -> Self {
+    fn new(path: PathBuf) -> Self {
         let data = parse_uevent(read_file(&path.join("uevent")));
         let name = data.get("POWER_SUPPLY_NAME").unwrap().clone();
         let dtype = data.get("POWER_SUPPLY_TYPE").unwrap().clone();
@@ -43,6 +43,7 @@ impl PowerDevice {
 
         self.calc_health_percentage();
         self.calc_power_consumption();
+        self.calc_time_remaining();
     }
 
     fn calc_health_percentage(&mut self) {
@@ -74,13 +75,54 @@ impl PowerDevice {
             .data
             .get("POWER_SUPPLY_POWER_NOW")
             .unwrap_or(&String::from("0"))
-            .clone()
             .parse()
             .unwrap_or(0);
 
         self.data.insert(
             String::from("POWER_SUPPLY_POWER_CONSUMPTION"),
-            format!("{}", round_2d(value as f32 / 1_000_000.0)),
+            format!("{:.2}", value as f32 / 1_000_000.0),
+        );
+    }
+
+    fn calc_time_remaining(&mut self) {
+        let energy_full = self
+            .data
+            .get("POWER_SUPPLY_ENERGY_FULL")
+            .unwrap_or(&String::from("0.0"))
+            .parse::<f32>()
+            .unwrap_or(0.0)
+            / 1_000_000.0;
+
+        let energy_now = self
+            .data
+            .get("POWER_SUPPLY_ENERGY_NOW")
+            .unwrap_or(&String::from("0.0"))
+            .parse::<f32>()
+            .unwrap_or(0.0)
+            / 1_000_000.0;
+
+        let power_now = self
+            .data
+            .get("POWER_SUPPLY_POWER_NOW")
+            .unwrap_or(&String::from("0.0"))
+            .parse::<f32>()
+            .unwrap_or(0.0)
+            / 1_000_000.0;
+
+        let (key, mut value) = match self.data.get("POWER_SUPPLY_STATUS").unwrap().as_str() {
+            "Discharging" => ("EMPTY", energy_now / power_now),
+            "Charging" => ("FULL", (energy_full - energy_now) / power_now),
+            _ => ("N/A", 0.0),
+        };
+
+        // convert hours to hours and minutes
+        let h = ((value / 1.0) - 0.5).round();
+        let r = (value % 1.0) * 60.0 / 100.0;
+        value = h + r;
+
+        self.data.insert(
+            format!("POWER_SUPPLY_TIME_REMAINING_UNTIL_{}", key),
+            format!("{:.2}", value),
         );
     }
 
@@ -107,7 +149,7 @@ impl std::fmt::Display for PowerDevice {
             if UNUSED_KEYS.contains(&key.as_str()) {
                 continue;
             }
-            output.push_str(&format!("{:<max_key_size$} = {}\n", formated_key, value));
+            output.push_str(&format!("\t{:<max_key_size$} = {}\n", formated_key, value));
         }
         write!(f, "{}", output)
     }
@@ -116,10 +158,6 @@ impl std::fmt::Display for PowerDevice {
 fn calc_percentage(part: f32, total: f32) -> f32 {
     let percentage = (part / total) * 100.0;
     (percentage * 100.0).round() / 100.0
-}
-
-fn round_2d(value: f32) -> f32 {
-    (value * 100.0).round() / 100.0
 }
 
 fn read_file(path: &PathBuf) -> String {
@@ -152,13 +190,11 @@ fn main() {
     let mut devices: Vec<PowerDevice> = vec![];
     if let Ok(entries) = fs::read_dir(power_supply) {
         for entry in entries {
-            if let Ok(entry) = entry {
-                devices.push(PowerDevice::from(entry.path()));
-            }
+            devices.push(PowerDevice::new(entry.unwrap().path()));
         }
     }
 
-    for device in &mut devices {
+    for device in devices.iter_mut() {
         device.calc_data();
         println!("{}", device);
     }
